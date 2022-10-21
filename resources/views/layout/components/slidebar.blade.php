f3d833219398860c57858802973bf05ae2bd00d6
Rugie Ann Barrameda <norugie015@gmail.com>
2022-10-20 21:55:32 -0700

    slidebar revision

--- resources/views/layout/components/slidebar.blade.php
+++ resources/views/layout/components/slidebar.blade.php
@@ -1,49 +1,31 @@
 <div id="highlighted">
-    <!-- Showshow - Slider Revolution see: plugins/slider-revolution/examples&sources for help invoke using data-toggle="slider-rev" options can be passed to the slider via HTML5 data- ie. data-startwidth="960" -->
-    <div class="slider-wrapper rev_slider_wrapper fullscreen-container bg-black" data-page-class="slider-appstrap-theme">
-        <div class="rev_slider fullscreenbanner" data-toggle="slider-rev" data-settings='{"startwidth":1100, "startheight":620, "delay":10000}'>
-            <ul>
-                <!-- SLIDE 1 -->
-                <li class="slide overlay overlay-op-4 overlay-gradient" data-transition="fadethroughdark" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="/assets/img/backgrounds/computer-screens.jpg" data-rotate="0" data-saveperformance="off" data-title="Slide" data-link="shop-list.html">
-                    <img src="/assets/img/slide-dummy.png" data-lazyload="/assets/img/backgrounds/computer-screens.jpg" alt="background image" data-bgposition="center top" data-bgfit="cover" data-bgparallax="off" class="rev-slidebg" data-no-retina />
-                    <!-- SLIDE 1 Content-->
-                    <div class="slide-content container" style="z-index: 10;">
-                        <!--elements within .slide-content are pushed below navbar on "behind"-->
-                        <!-- Layer 1 -->
-                        <div class="tp-caption text-grey" data-x="['left','left','left','left']" data-hoffset="['0','0','0','40']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['120','120','120','120']" data-fontsize="['52','52','52','52']" data-lineheight="['52','52','52','52']" data-width="420" data-height="none" data-whitespace="normal" data-type="text" data-basealign="slide" data-responsive_offset="on" data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1200,"to":"o:1;","delay":1300,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"opacity:0;","ease":"nothing"}]' data-textAlign="left"> <span class="font-weight-bold text-uppercase text-white">CMSD<span class="text-primary">82</span></span>
-                        </div>
-                        <!-- Layer 3 -->
-                        <div class="tp-caption text-grey" data-x="['left','left','left','left']" data-hoffset="['0','0','0','40']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['60','60','60','60']" data-fontsize="['20','20','20','30']" data-width="none" data-height="none" data-whitespace="nowrap" data-type="text" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":0,"speed":1200,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","to":"o:1;","delay":1400},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]' data-textAlign="['center','center','center','center']"> Coast Mountain School District No. 82
-                        </div>
-                        {{-- <!-- Layer 3 -->
-                        <div class="tp-caption rs-parallaxlevel-8" data-frames='[{"from":"o:0;sX:3;sY:3;","speed":2000,"to":"o:0.20;sX:3;sY:3;","delay":1800,"ease":"default"},{"delay":"wait","speed":1200,"to":"x:[100%];","ease":"Power3.easeInOut"}]' data-x="left" data-y="bottom" data-hoffset="-200" data-voffset="0" data-width="none" data-height="none" data-type="image" data-basealign="slide"> <i class="fa fa-bullseye icon-20x icon-bg op-1 text-white"></i>
-                        </div> --}}
-                        <!-- Layer 4 -->
-                        <div class="tp-caption tp-resizeme bg-primary-bright" data-frames='[{"from":"x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"x:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]' data-x="['left','left','left','left']" data-hoffset="['0','0','0','40']" data-y="bottom" data-voffset="100" data-width="380" data-height="4" data-basealign="slide"></div>
-                    </div>
-                </li>
-
-                <li class="slide overlay overlay-op-4 overlay-gradient" data-transition="fadethroughdark" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="/assets/img/backgrounds/keyboard-coffee.jpg" data-rotate="0" data-saveperformance="off" data-title="Slide" data-link="shop-list.html">
-                    <img src="/assets/img/slide-dummy.png" data-lazyload="/assets/img/backgrounds/keyboard-coffee.jpg" alt="background image" data-bgposition="center top" data-bgfit="cover" data-bgparallax="off" class="rev-slidebg" data-no-retina />
-                    <!-- SLIDE 1 Content-->
-                    <div class="slide-content container" style="z-index: 10;">
-                        <!--elements within .slide-content are pushed below navbar on "behind"-->
-                        <!-- Layer 1 -->
-                        <div class="tp-caption text-grey" data-x="['left','left','left','left']" data-hoffset="['0','0','0','40']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['120','120','120','120']" data-fontsize="['52','52','52','52']" data-lineheight="['52','52','52','52']" data-width="420" data-height="none" data-whitespace="normal" data-type="text" data-basealign="slide" data-responsive_offset="on" data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1200,"to":"o:1;","delay":1300,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"opacity:0;","ease":"nothing"}]' data-textAlign="left"> <span class="font-weight-bold text-uppercase text-white">CMSD<span class="text-primary">82</span></span>
-                        </div>
-                        <!-- Layer 3 -->
-                        <div class="tp-caption text-grey" data-x="['left','left','left','left']" data-hoffset="['0','0','0','40']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['60','60','60','60']" data-fontsize="['20','20','20','30']" data-width="none" data-height="none" data-whitespace="nowrap" data-type="text" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":0,"speed":1200,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","to":"o:1;","delay":1400},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]' data-textAlign="['center','center','center','center']"> Coast Mountain School District No. 82
-                        </div>
-                        {{-- <!-- Layer 3 -->
-                        <div class="tp-caption rs-parallaxlevel-8" data-frames='[{"from":"o:0;sX:3;sY:3;","speed":2000,"to":"o:0.20;sX:3;sY:3;","delay":1800,"ease":"default"},{"delay":"wait","speed":1200,"to":"x:[100%];","ease":"Power3.easeInOut"}]' data-x="left" data-y="bottom" data-hoffset="-200" data-voffset="0" data-width="none" data-height="none" data-type="image" data-basealign="slide"> <i class="fa fa-bullseye icon-20x icon-bg op-1 text-white"></i>
-                        </div> --}}
-                        <!-- Layer 4 -->
-                        <div class="tp-caption tp-resizeme bg-primary-bright" data-frames='[{"from":"x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"x:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]' data-x="['left','left','left','left']" data-hoffset="['0','0','0','40']" data-y="bottom" data-voffset="100" data-width="380" data-height="4" data-basealign="slide"></div>
-                    </div>
-                </li>
-            </ul>
-            <div class="tp-bannertimer tp-bottom"></div>
+    <div class="mt-0 owl-dots-center owl-nav-over owl-nav-over-lg owl-nav-over-hover" data-toggle="owl-carousel" data-owl-carousel-settings='{"items":1, "center":true, "autoplay":true, "loop":true, "dots":true, "nav":true, "animateOut":"fadeOut"}'>
+        <div class="item">
+            <img src="/assets/img/backgrounds/computer-screens.jpg" alt="Project 1" class="img-fluid" />
+            <div class="bg-light p-3">
+                <h4 class="my-0">
+                    Project 1
+                </h4>
+                <p class="mb-0">Project 1 description text.</p>
+            </div>
         </div>
-        <!--end of tp-banner-->
+        <div class="item">
+            <img src="/assets/img/backgrounds/index-corporate-banner.jpg" alt="Project 1" class="img-fluid" />
+            <div class="bg-light p-3">
+                <h4 class="my-0">
+                    Project 1
+                </h4>
+                <p class="mb-0">Project 1 description text.</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="/assets/img/backgrounds/keyboard-coffee.jpg" alt="Project 1" class="img-fluid" />
+            <div class="bg-light p-3">
+                <h4 class="my-0">
+                    Project 1
+                </h4>
+                <p class="mb-0">Project 1 description text.</p>
+            </div>
+        </div>
     </div>
 </div>
