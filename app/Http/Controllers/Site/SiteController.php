935d1f59077e98c2a71588a4e2287562a2560cc4
Rugie Ann Barrameda <norugie015@gmail.com>
2022-11-08 15:02:26 -0800

    error page layouts and routing setup

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -9,16 +9,13 @@ class SiteController extends Controller
 {
     public function indexView ()
     {
-        return view ( 'index' );
-    }
-
-    public function errorView ()
-    {
-        return view ( 'error' );
+        return view ('index');
     }
 
     public function pageRouterView (String $page)
     {
-
+        if (\View::exists($page)) {
+            return view ($page);
+        } else return view ('404');
     }
 }
--- resources/views/restricted.blade.php
+++ resources/views/restricted.blade.php
@@ -8,15 +8,15 @@
                     CMSD<span class="text-primary">82</span>
                 </h3>
                 <h2 class="error-code text-center text-xs-x6 text-md-x6 op-9">
-                    Error <i class="fa fa-file text-primary text-xs-x6 text-md-x6"></i>
+                    403 <i class="fa fa-file text-primary text-xs-x6 text-md-x6"></i>
                 </h2>
                 <h2 class="error-message text-center text-xs-x3 text-md-x2">
-                    An error occured while processing your request.
+                    This page is a restricted page.
                 </h2>
-                <p class="error-details text-center text-xs-x2">Something is broken. Please contact us at itsupport@cmsd.bc.ca to let us know of the error that has occured. Sorry for the inconvenience.</p>
-                <div class="text-center mt-1">
-                    <a href="/" class="mb-1 btn btn-xlg btn-outline-primary">Primary</a>
-                </div>
+                <p class="error-details text-center text-xs-x2">You do not have the required permission to access this page.</p>
+            </div>
+            <div class="text-center mt-1">
+                <a href="/" class="mb-1 btn btn-xlg btn-outline-primary">Return to homepage</a>
             </div>
         </div>
     </div>
